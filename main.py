b887c79972803ceb04a3e6041c87cd09df0b6778
fathimasilviyak <silviyakvm@gmail.com>
2023-03-25 12:19:07 +0530

    Create a class for all the things related to snake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# Create a snake body
-from turtle import Turtle, Screen
+from snake import Snake
+from turtle import Screen
 import time
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -15,12 +15,7 @@ segments = []
 # to stop displaying things on the screen. It will again start to display when screen.update is called.
 screen.tracer(0)
 
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 is_game_on = True
 
@@ -28,13 +23,7 @@ is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.1)
-    for segment_num in range(len(segments)-1, 0, -1):
-        x_position = segments[segment_num-1].xcor()
-        y_position = segments[segment_num-1].ycor()
-        segments[segment_num].goto(x_position, y_position)
-    segments[0].forward(20)
-    segments[0].left(90)
-
+    snake.move()
 
 
 
