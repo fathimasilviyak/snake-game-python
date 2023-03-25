b9baf0b95a60297f2e08f4668e98f6041742f672
fathimasilviyak <silviyakvm@gmail.com>
2023-03-25 09:48:33 +0530

    Create snake body

---
+++ main.py
@@ -0,0 +1,17 @@
+# Create a snake body
+from turtle import Turtle, Screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+# create snake body
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+g
+
+screen.exitonclick()
