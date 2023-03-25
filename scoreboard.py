145202756dddf41e7682b095889e8e5287f0710b
fathimasilviyak <silviyakvm@gmail.com>
2023-03-25 20:33:18 +0530

    Detect collision with wall

--- scoreboard.py
+++ scoreboard.py
@@ -21,3 +21,8 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER!", move=False, align=ALIGNMENT, font=FONT)
+
